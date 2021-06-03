905f96819c0e1dd472c6dcc6e181584a34c94ca9
Almaz Yessenyazov <mr.esenyazov@gmail.com>
2021-06-03 15:31:21 +0600

    Changed database config

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -87,12 +87,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(default=os.environ.get('HEROKU_POSTGRESQL_COPPER_URL'))
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
